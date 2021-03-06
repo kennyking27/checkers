f7d543c880f1989842c3fd48dc223bdc041a6b10
EC2 Default User <ec2-user@ip-172-31-42-32.ec2.internal>
2021-03-06 06:25:46 +0000

    adding file

---
+++ run.java
@@ -0,0 +1,10 @@
+
+public class run {
+	
+	public static void main(String [] args){
+		//Player p1 = new Player("Pleyer One", 0);
+		//Player p2 = new Player("Player Two", 0);
+		Board b = new Board();
+		b.printGameBoard();
+	}
+}
